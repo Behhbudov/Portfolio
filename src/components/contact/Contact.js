a3b39b6843d08f34be68d1e904c0a251ba9fda5c
Murad Behbudov <84980601+behhbudov@users.noreply.github.com>
2023-08-30 10:35:37 +0400

    commit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -13,9 +13,9 @@ const Contact = () => {
       <div className="contact__details">
         <h3>Get In Touch</h3>
         <p>
-          Although I’m not currently looking for any new opportunities, my inbox
-          is always open. Whether you have a question or just want to say hi,
-          I’ll try my best to get back to you!
+          As well I’m currently looking for any new opportunities, my inbox is
+          always open. Whether you have a question or just want to say hi, I’ll
+          try my best to get back to you!
         </p>
         <a href="mailto:murad.5behbudov@gmail.com">Say Hello</a>
       </div>
